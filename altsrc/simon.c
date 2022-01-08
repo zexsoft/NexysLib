3eadb334ccd4829df5309f6cfd6f84d7ca95fa24
Hexagon-77 <95217387+Hexagon-77@users.noreply.github.com>
2022-01-08 18:05:50 +0200

    Updated 'Simon says' example

--- altsrc/simon.c
+++ altsrc/simon.c
@@ -1,7 +1,12 @@
 #include "nexys_driver.c"
 #include "disp_driver.c"
 
-// 'Simon says' for Nexys A7
+#define choices 4
+#define bounce_delay 1000000
+#define short_delay 5000000
+#define long_delay 8500000
+
+// 'Simon says' program for Nexys A7
 
 int delay;
 
@@ -14,15 +19,15 @@ int main()
     disp_hex_value(0);
     LEDs_value(0);
 
-    int seed = 7;
-    int n = 0, v[105];
+    int seed = 7;       // Seed for random (randomized through delays)
+    int n = 0, v[1005];
     while (1) { 
         // Generate inital sequence
-        for(int i=0;i<=8500000;i++);
+        for(int i=0;i<=long_delay;i++);
         disp_hex_value(0);
         n = 1;
         srand(seed);
-        v[0] = 1 << (rand() % 4);
+        v[0] = 1 << (rand() % choices);
 
         // Main loop
         int ok = 1;
@@ -30,24 +35,24 @@ int main()
             // Show sequence
             LEDs_value(0);
             disp_hex_value(0xBE6);
-            for(int i=0;i<=8500000;i++);
+            for(int i=0;i<=long_delay;i++);
             for (int i=0;i<n;i++) {
                 LEDs_value(v[i]);
-                disp_dec_value(i+1);
+                disp_dec_value(i+1);                    // Count on display
 
-                for(int j=0;j<=5000000;j++);
+                for(int j=0;j<=short_delay;j++);
             }
             LEDs_value(0);
             
             disp_hex_value(0x5EC); 
-            for(int j=0;j<=8500000;j++);
+            for(int j=0;j<=long_delay;j++);
             // Check sequence
             for (int i=0;i<n;i++) {
                 int r = switch_read();
-                disp_dec_value(i+1);
+                disp_dec_value(i+1);                    // Count on display
 
-                while (r == switch_read()) seed++;
-                for (int j=1;j<=1000005;j++);       // Delay to prevent bounce
+                while (r == switch_read()) seed++;      // Seed random through delay
+                for (int j=1;j<=bounce_delay;j++);      // Delay to prevent bounce
 
                 // Get changed switch & check it
                 int val = r ^ switch_read();
@@ -67,7 +72,7 @@ int main()
             for(int j=0;j<=8500000;j++);
             
             // Add to sequence
-            v[n++] = 1 << (rand() % 4);
+            v[n++] = 1 << (rand() % choices);
             srand(seed);	// Reseed random
         }
     }
