f65b676ee251b8e5c7adbd38de3589030325d148
Hexagon-77 <95217387+Hexagon-77@users.noreply.github.com>
2022-01-08 17:30:12 +0200

    Updated example program

--- src/main.c
+++ src/main.c
@@ -1,13 +1,15 @@
 #include "nexys_driver.c"
 #include "disp_driver.c"
 
+// Example program for Nexys A7
+
 int main()
 {
     baseIO_init();
     disp_init();
     
     while (1) { 
-        disp_hex_value(switch_read());
+        disp_dec_value(switch_read());
     }
     
     return 0;
